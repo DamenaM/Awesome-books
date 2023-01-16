fdca35db0460597b241a8651af06b3b6629eddd7
Mtab <107020397+DamenaM@users.noreply.github.com>
2023-01-16 18:41:47 +0300

    add aouthor

--- main.js
+++ main.js
@@ -1,22 +1,58 @@
-const mylist = document.getElementById('books');
-const clickbutton = document.getElementById('clickbutton');
+// const mylist = document.getElementById('books');
+// const clickbutton = document.getElementById('clickbutton');
 
-const title = document.getElementById('titleid');
-const author = document.getElementById('authorid');
+// const { div } = require("prelude-ls");
 
-const lititle = document.createElement('div');
-const liauthor = document.createElement('div');
-const btn = document.createElement('button');
+// const { div } = require("prelude-ls");
 
-clickbutton.addEventListener('click', () => {
-  lititle.setAttribute('titleid', title.value);
-  liauthor.setAttribute('authorid', author.value);
+// const title = document.getElementById('titleid');
+// const author = document.getElementById('authorid');
 
-  btn.textContent = 'Remove';
-  lititle.appendChild(document.createTextNode(title.value));
-  liauthor.appendChild(document.createTextNode(author.value));
+// const lititle = document.createElement('div');
+// const liauthor = document.createElement('div');
+// const btn = document.createElement('button');
 
-  mylist.appendChild(lititle);
-  mylist.appendChild(liauthor);
-  mylist.appendChild(btn);
-});
+
+// clickbutton.addEventListener('click', () => {
+
+//   lititle.setAttribute('titleid', title.value);
+//   liauthor.setAttribute('authorid', author.value);
+
+//   btn.textContent = 'Remove';
+//   lititle.appendChild(document.createTextNode(title.value));
+//   liauthor.appendChild(document.createTextNode(author.value));
+
+//   mylist.appendChild(lititle);
+//   mylist.appendChild(liauthor);
+//   mylist.appendChild(btn);
+// });
+// ----------------//------------------------------------------
+
+let mylist = document.getElementById('books');
+let mydiv= document.createElement('div');
+
+// const clickbutton = document.getElementById('clickbutton');
+document.getElementById('clickbutton').addEventListener('click',addAuthor);
+
+
+function addAuthor() {
+  let title = document.getElementById('titleid').value;
+  let author = document.getElementById('authorid').value;
+  let bookDetails=`<div>
+    <div>
+    <div> ${title}</div>
+    <div> ${author}</div> 
+    <div>
+    <button class="clickdelete" type="button"> Delete </button>
+     <div> <br>
+    </div>`;
+
+// mylist.insertAdjacentHTML('beforeend',bookdetails);
+ 
+};
+
+
+
+
+
+
